83dfbf7edd2d85179b46ab45caf68f78eb0d1312
jisu <js305386@gmail.com>
2021-12-06 09:25:46 +0900

    첫번째 커밋

---
+++ src/test/java/dev/mvc/wehealth_v1sbm3c/WehealthV1sbm3cApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mvc.wehealth_v1sbm3c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WehealthV1sbm3cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
